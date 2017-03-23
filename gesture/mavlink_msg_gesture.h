4f7b69111e5df5b4058bcda0ed2079b3f2b7d447
bdai <bdai1412@gmail.com>
2017-03-23 17:23:56 +0800

    add att_pos_vel_mocap message

--- gesture/mavlink_msg_gesture.h
+++ gesture/mavlink_msg_gesture.h
@@ -4,18 +4,17 @@
 
 MAVPACKED(
 typedef struct __mavlink_gesture_t {
- uint64_t time_usec; /*< Timestamp (micros since boot or Unix epoch)*/
- float reserved[3]; /*< Reserved use*/
- uint8_t gesture_num; /*< The num of gesture identification*/
+ float reserved[3]; /*< reserved value*/
+ uint8_t gesture_num; /*<  num of gesture identification*/
 }) mavlink_gesture_t;
 
-#define MAVLINK_MSG_ID_GESTURE_LEN 21
-#define MAVLINK_MSG_ID_GESTURE_MIN_LEN 21
-#define MAVLINK_MSG_ID_183_LEN 21
-#define MAVLINK_MSG_ID_183_MIN_LEN 21
+#define MAVLINK_MSG_ID_GESTURE_LEN 13
+#define MAVLINK_MSG_ID_GESTURE_MIN_LEN 13
+#define MAVLINK_MSG_ID_183_LEN 13
+#define MAVLINK_MSG_ID_183_MIN_LEN 13
 
-#define MAVLINK_MSG_ID_GESTURE_CRC 169
-#define MAVLINK_MSG_ID_183_CRC 169
+#define MAVLINK_MSG_ID_GESTURE_CRC 39
+#define MAVLINK_MSG_ID_183_CRC 39
 
 #define MAVLINK_MSG_GESTURE_FIELD_RESERVED_LEN 3
 
@@ -23,19 +22,17 @@ typedef struct __mavlink_gesture_t {
 #define MAVLINK_MESSAGE_INFO_GESTURE { \
 	183, \
 	"GESTURE", \
-	3, \
-	{  { "time_usec", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_gesture_t, time_usec) }, \
-         { "reserved", NULL, MAVLINK_TYPE_FLOAT, 3, 8, offsetof(mavlink_gesture_t, reserved) }, \
-         { "gesture_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 20, offsetof(mavlink_gesture_t, gesture_num) }, \
+	2, \
+	{  { "reserved", NULL, MAVLINK_TYPE_FLOAT, 3, 0, offsetof(mavlink_gesture_t, reserved) }, \
+         { "gesture_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 12, offsetof(mavlink_gesture_t, gesture_num) }, \
          } \
 }
 #else
 #define MAVLINK_MESSAGE_INFO_GESTURE { \
 	"GESTURE", \
-	3, \
-	{  { "time_usec", NULL, MAVLINK_TYPE_UINT64_T, 0, 0, offsetof(mavlink_gesture_t, time_usec) }, \
-         { "reserved", NULL, MAVLINK_TYPE_FLOAT, 3, 8, offsetof(mavlink_gesture_t, reserved) }, \
-         { "gesture_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 20, offsetof(mavlink_gesture_t, gesture_num) }, \
+	2, \
+	{  { "reserved", NULL, MAVLINK_TYPE_FLOAT, 3, 0, offsetof(mavlink_gesture_t, reserved) }, \
+         { "gesture_num", NULL, MAVLINK_TYPE_UINT8_T, 0, 12, offsetof(mavlink_gesture_t, gesture_num) }, \
          } \
 }
 #endif
@@ -46,23 +43,20 @@ typedef struct __mavlink_gesture_t {
  * @param component_id ID of this component (e.g. 200 for IMU)
  * @param msg The MAVLink message to compress the data into
  *
- * @param time_usec Timestamp (micros since boot or Unix epoch)
- * @param gesture_num The num of gesture identification
- * @param reserved Reserved use
+ * @param gesture_num  num of gesture identification
+ * @param reserved reserved value
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_gesture_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-						       uint64_t time_usec, uint8_t gesture_num, const float *reserved)
+						       uint8_t gesture_num, const float *reserved)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
 	char buf[MAVLINK_MSG_ID_GESTURE_LEN];
-	_mav_put_uint64_t(buf, 0, time_usec);
-	_mav_put_uint8_t(buf, 20, gesture_num);
-	_mav_put_float_array(buf, 8, reserved, 3);
+	_mav_put_uint8_t(buf, 12, gesture_num);
+	_mav_put_float_array(buf, 0, reserved, 3);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_GESTURE_LEN);
 #else
 	mavlink_gesture_t packet;
-	packet.time_usec = time_usec;
 	packet.gesture_num = gesture_num;
 	mav_array_memcpy(packet.reserved, reserved, sizeof(float)*3);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_GESTURE_LEN);
@@ -78,24 +72,21 @@ static inline uint16_t mavlink_msg_gesture_pack(uint8_t system_id, uint8_t compo
  * @param component_id ID of this component (e.g. 200 for IMU)
  * @param chan The MAVLink channel this message will be sent over
  * @param msg The MAVLink message to compress the data into
- * @param time_usec Timestamp (micros since boot or Unix epoch)
- * @param gesture_num The num of gesture identification
- * @param reserved Reserved use
+ * @param gesture_num  num of gesture identification
+ * @param reserved reserved value
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_gesture_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
 							   mavlink_message_t* msg,
-						           uint64_t time_usec,uint8_t gesture_num,const float *reserved)
+						           uint8_t gesture_num,const float *reserved)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
 	char buf[MAVLINK_MSG_ID_GESTURE_LEN];
-	_mav_put_uint64_t(buf, 0, time_usec);
-	_mav_put_uint8_t(buf, 20, gesture_num);
-	_mav_put_float_array(buf, 8, reserved, 3);
+	_mav_put_uint8_t(buf, 12, gesture_num);
+	_mav_put_float_array(buf, 0, reserved, 3);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_GESTURE_LEN);
 #else
 	mavlink_gesture_t packet;
-	packet.time_usec = time_usec;
 	packet.gesture_num = gesture_num;
 	mav_array_memcpy(packet.reserved, reserved, sizeof(float)*3);
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_GESTURE_LEN);
@@ -115,7 +106,7 @@ static inline uint16_t mavlink_msg_gesture_pack_chan(uint8_t system_id, uint8_t 
  */
 static inline uint16_t mavlink_msg_gesture_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_gesture_t* gesture)
 {
-	return mavlink_msg_gesture_pack(system_id, component_id, msg, gesture->time_usec, gesture->gesture_num, gesture->reserved);
+	return mavlink_msg_gesture_pack(system_id, component_id, msg, gesture->gesture_num, gesture->reserved);
 }
 
 /**
@@ -129,30 +120,27 @@ static inline uint16_t mavlink_msg_gesture_encode(uint8_t system_id, uint8_t com
  */
 static inline uint16_t mavlink_msg_gesture_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_gesture_t* gesture)
 {
-	return mavlink_msg_gesture_pack_chan(system_id, component_id, chan, msg, gesture->time_usec, gesture->gesture_num, gesture->reserved);
+	return mavlink_msg_gesture_pack_chan(system_id, component_id, chan, msg, gesture->gesture_num, gesture->reserved);
 }
 
 /**
  * @brief Send a gesture message
  * @param chan MAVLink channel to send the message
  *
- * @param time_usec Timestamp (micros since boot or Unix epoch)
- * @param gesture_num The num of gesture identification
- * @param reserved Reserved use
+ * @param gesture_num  num of gesture identification
+ * @param reserved reserved value
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
-static inline void mavlink_msg_gesture_send(mavlink_channel_t chan, uint64_t time_usec, uint8_t gesture_num, const float *reserved)
+static inline void mavlink_msg_gesture_send(mavlink_channel_t chan, uint8_t gesture_num, const float *reserved)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
 	char buf[MAVLINK_MSG_ID_GESTURE_LEN];
-	_mav_put_uint64_t(buf, 0, time_usec);
-	_mav_put_uint8_t(buf, 20, gesture_num);
-	_mav_put_float_array(buf, 8, reserved, 3);
+	_mav_put_uint8_t(buf, 12, gesture_num);
+	_mav_put_float_array(buf, 0, reserved, 3);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GESTURE, buf, MAVLINK_MSG_ID_GESTURE_MIN_LEN, MAVLINK_MSG_ID_GESTURE_LEN, MAVLINK_MSG_ID_GESTURE_CRC);
 #else
 	mavlink_gesture_t packet;
-	packet.time_usec = time_usec;
 	packet.gesture_num = gesture_num;
 	mav_array_memcpy(packet.reserved, reserved, sizeof(float)*3);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GESTURE, (const char *)&packet, MAVLINK_MSG_ID_GESTURE_MIN_LEN, MAVLINK_MSG_ID_GESTURE_LEN, MAVLINK_MSG_ID_GESTURE_CRC);
@@ -167,7 +155,7 @@ static inline void mavlink_msg_gesture_send(mavlink_channel_t chan, uint64_t tim
 static inline void mavlink_msg_gesture_send_struct(mavlink_channel_t chan, const mavlink_gesture_t* gesture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_gesture_send(chan, gesture->time_usec, gesture->gesture_num, gesture->reserved);
+    mavlink_msg_gesture_send(chan, gesture->gesture_num, gesture->reserved);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GESTURE, (const char *)gesture, MAVLINK_MSG_ID_GESTURE_MIN_LEN, MAVLINK_MSG_ID_GESTURE_LEN, MAVLINK_MSG_ID_GESTURE_CRC);
 #endif
@@ -181,17 +169,15 @@ static inline void mavlink_msg_gesture_send_struct(mavlink_channel_t chan, const
   is usually the receive buffer for the channel, and allows a reply to an
   incoming message with minimum stack space usage.
  */
-static inline void mavlink_msg_gesture_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint64_t time_usec, uint8_t gesture_num, const float *reserved)
+static inline void mavlink_msg_gesture_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t gesture_num, const float *reserved)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
 	char *buf = (char *)msgbuf;
-	_mav_put_uint64_t(buf, 0, time_usec);
-	_mav_put_uint8_t(buf, 20, gesture_num);
-	_mav_put_float_array(buf, 8, reserved, 3);
+	_mav_put_uint8_t(buf, 12, gesture_num);
+	_mav_put_float_array(buf, 0, reserved, 3);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GESTURE, buf, MAVLINK_MSG_ID_GESTURE_MIN_LEN, MAVLINK_MSG_ID_GESTURE_LEN, MAVLINK_MSG_ID_GESTURE_CRC);
 #else
 	mavlink_gesture_t *packet = (mavlink_gesture_t *)msgbuf;
-	packet->time_usec = time_usec;
 	packet->gesture_num = gesture_num;
 	mav_array_memcpy(packet->reserved, reserved, sizeof(float)*3);
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GESTURE, (const char *)packet, MAVLINK_MSG_ID_GESTURE_MIN_LEN, MAVLINK_MSG_ID_GESTURE_LEN, MAVLINK_MSG_ID_GESTURE_CRC);
@@ -205,33 +191,23 @@ static inline void mavlink_msg_gesture_send_buf(mavlink_message_t *msgbuf, mavli
 
 
 /**
- * @brief Get field time_usec from gesture message
- *
- * @return Timestamp (micros since boot or Unix epoch)
- */
-static inline uint64_t mavlink_msg_gesture_get_time_usec(const mavlink_message_t* msg)
-{
-	return _MAV_RETURN_uint64_t(msg,  0);
-}
-
-/**
  * @brief Get field gesture_num from gesture message
  *
- * @return The num of gesture identification
+ * @return  num of gesture identification
  */
 static inline uint8_t mavlink_msg_gesture_get_gesture_num(const mavlink_message_t* msg)
 {
-	return _MAV_RETURN_uint8_t(msg,  20);
+	return _MAV_RETURN_uint8_t(msg,  12);
 }
 
 /**
  * @brief Get field reserved from gesture message
  *
- * @return Reserved use
+ * @return reserved value
  */
 static inline uint16_t mavlink_msg_gesture_get_reserved(const mavlink_message_t* msg, float *reserved)
 {
-	return _MAV_RETURN_float_array(msg, reserved, 3,  8);
+	return _MAV_RETURN_float_array(msg, reserved, 3,  0);
 }
 
 /**
@@ -243,7 +219,6 @@ static inline uint16_t mavlink_msg_gesture_get_reserved(const mavlink_message_t*
 static inline void mavlink_msg_gesture_decode(const mavlink_message_t* msg, mavlink_gesture_t* gesture)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-	gesture->time_usec = mavlink_msg_gesture_get_time_usec(msg);
 	mavlink_msg_gesture_get_reserved(msg, gesture->reserved);
 	gesture->gesture_num = mavlink_msg_gesture_get_gesture_num(msg);
 #else
--- gesture/testsuite.h
+++ gesture/testsuite.h
@@ -24,6 +24,67 @@ static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_me
 #include "../common/testsuite.h"
 
 
+static void mavlink_test_att_pos_vel_mocap(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+	mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_ATT_POS_VEL_MOCAP >= 256) {
+        	return;
+        }
+#endif
+	mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+	mavlink_att_pos_vel_mocap_t packet_in = {
+		93372036854775807ULL,{ 73.0, 74.0, 75.0, 76.0 },185.0,213.0,241.0,269.0,297.0,325.0
+    };
+	mavlink_att_pos_vel_mocap_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.time_usec = packet_in.time_usec;
+        packet1.x = packet_in.x;
+        packet1.y = packet_in.y;
+        packet1.z = packet_in.z;
+        packet1.Vx = packet_in.Vx;
+        packet1.Vy = packet_in.Vy;
+        packet1.Vz = packet_in.Vz;
+        
+        mav_array_memcpy(packet1.q, packet_in.q, sizeof(float)*4);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_ATT_POS_VEL_MOCAP_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_ATT_POS_VEL_MOCAP_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_att_pos_vel_mocap_encode(system_id, component_id, &msg, &packet1);
+	mavlink_msg_att_pos_vel_mocap_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_att_pos_vel_mocap_pack(system_id, component_id, &msg , packet1.time_usec , packet1.q , packet1.x , packet1.y , packet1.z , packet1.Vx , packet1.Vy , packet1.Vz );
+	mavlink_msg_att_pos_vel_mocap_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_att_pos_vel_mocap_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_usec , packet1.q , packet1.x , packet1.y , packet1.z , packet1.Vx , packet1.Vy , packet1.Vz );
+	mavlink_msg_att_pos_vel_mocap_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+        	comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+	mavlink_msg_att_pos_vel_mocap_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+	mavlink_msg_att_pos_vel_mocap_send(MAVLINK_COMM_1 , packet1.time_usec , packet1.q , packet1.x , packet1.y , packet1.z , packet1.Vx , packet1.Vy , packet1.Vz );
+	mavlink_msg_att_pos_vel_mocap_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+}
+
 static void mavlink_test_gesture(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -36,11 +97,10 @@ static void mavlink_test_gesture(uint8_t system_id, uint8_t component_id, mavlin
         uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
         uint16_t i;
 	mavlink_gesture_t packet_in = {
-		93372036854775807ULL,{ 73.0, 74.0, 75.0 },65
+		{ 17.0, 18.0, 19.0 },41
     };
 	mavlink_gesture_t packet1, packet2;
         memset(&packet1, 0, sizeof(packet1));
-        packet1.time_usec = packet_in.time_usec;
         packet1.gesture_num = packet_in.gesture_num;
         
         mav_array_memcpy(packet1.reserved, packet_in.reserved, sizeof(float)*3);
@@ -57,12 +117,12 @@ static void mavlink_test_gesture(uint8_t system_id, uint8_t component_id, mavlin
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-	mavlink_msg_gesture_pack(system_id, component_id, &msg , packet1.time_usec , packet1.gesture_num , packet1.reserved );
+	mavlink_msg_gesture_pack(system_id, component_id, &msg , packet1.gesture_num , packet1.reserved );
 	mavlink_msg_gesture_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-	mavlink_msg_gesture_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.time_usec , packet1.gesture_num , packet1.reserved );
+	mavlink_msg_gesture_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.gesture_num , packet1.reserved );
 	mavlink_msg_gesture_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
@@ -75,13 +135,14 @@ static void mavlink_test_gesture(uint8_t system_id, uint8_t component_id, mavlin
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
         
         memset(&packet2, 0, sizeof(packet2));
-	mavlink_msg_gesture_send(MAVLINK_COMM_1 , packet1.time_usec , packet1.gesture_num , packet1.reserved );
+	mavlink_msg_gesture_send(MAVLINK_COMM_1 , packet1.gesture_num , packet1.reserved );
 	mavlink_msg_gesture_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 }
 
 static void mavlink_test_gesture(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
+	mavlink_test_att_pos_vel_mocap(system_id, component_id, last_msg);
 	mavlink_test_gesture(system_id, component_id, last_msg);
 }
 
